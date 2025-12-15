04213b02deb822c0715ecf559c03182adaa89214
Francisco Braga <franciscocbbraga@gmail.com>
2025-12-15 16:01:36 -0300

    trabalhando na adição da seleção dos pontos de atributos, tendo uma interação com o usuário

--- src/main/java/com/progressquest/model/Attributes.java
+++ src/main/java/com/progressquest/model/Attributes.java
@@ -16,6 +16,10 @@ public class Attributes {
 
     public Attributes() {
         stats = new LinkedHashMap<>();
+        reset();
+    }
+
+    public void reset() {
         stats.put("STR", 0);
         stats.put("CON", 0);
         stats.put("DEX", 0);
@@ -24,22 +28,42 @@ public class Attributes {
         stats.put("CHA", 0);
     }
 
-    public void roll(){
+    public void setAll(int value) {
         for (String key : stats.keySet()) {
+            stats.put(key, value);
+        }
+    }
+
+    public void increment(String key) {
+        stats.put(key, stats.getOrDefault(key, 0) + 1);
+    }
+
+    public void decrement(String key) {
+        int val = stats.getOrDefault(key, 0);
+        if (val > 0) {
+            stats.put(key, val - 1);
+        }
+    }
+
+    public void roll() {
+        for (String key : stats.keySet()) {
+            // Rola 3 a 18
             stats.put(key, 3 + rand.nextInt(16));
         }
     }
 
+
+    //mantive para caso seja utilizado em algum npc no futuro
     public void increaseRandom() {
         String[] keys = stats.keySet().toArray(new String[0]);
         String key = keys[rand.nextInt(keys.length)];
         stats.put(key, stats.get(key) + 1);
     }
 
-    public int get(String key) {return stats.getOrDefault(key, 0);}
-    public Map<String, Integer> getAll(){return stats;}
+    public int get(String key) { return stats.getOrDefault(key, 0); }
+    public Map<String, Integer> getAll() { return stats; }
 
-    public int getTotal(){
+    public int getTotal() {
         return stats.values().stream().mapToInt(Integer::intValue).sum();
     }
 }
